cee9a64ad71d057cb05f25e93aa7220e56d93949
ddobson <ddobson445@gmail.com>
2017-01-15 08:19:47 -0500

    Integrate babel with gulp Setup gulp to watch for changes in js using babel

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,39 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var babel = require('gulp-babel');
 
 gulp.task('default', function() {
   return console.log('Gulp is running!');
 });
 
+// SASS
+var sassPaths = {
+  scss: './scss/**/*.scss',
+  css: './css/'
+};
+
 gulp.task('sass', function(){
-  return gulp.src('./scss/**/*.scss')
+  return gulp.src(sassPaths.scss)
     .pipe(sass.sync())
-    .pipe(gulp.dest('./css/'));
+    .pipe(gulp.dest(sassPaths.css));
+});
+
+// BABEL
+var babelPaths ={
+  buildPath: './build',
+  JSPath: './js/**/*.js'
+};
+
+gulp.task('babel', function() {
+  return gulp.src(babelPaths.JSPath)
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest(babelPaths.buildPath));
 });
 
 gulp.task('watch', function(){
-  gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch(['./scss/**/*.scss',babelPaths.JSPath], ['sass','babel']);
   // Other watchers
 });
