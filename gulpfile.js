dd2557953cedeab7795a499dfa4ce847c87e1ce7
ddobson <ddobson445@gmail.com>
2017-01-15 00:19:09 -0500

    Configuration changes Includes gulp, gulp-sass, scss dir, jshintrc changes - Added `gulp` & `gulp-sass` - Created scss directory and main.scss - Added node to jshintrc

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('default', function() {
+  return console.log('Gulp is running!');
+});
+
+gulp.task('sass', function(){
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass.sync())
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./scss/**/*.scss', ['sass']);
+  // Other watchers
+});
